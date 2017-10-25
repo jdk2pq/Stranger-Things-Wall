ca28cfe1c4c7fafd5ec8474b1967b76201479b43
Jake Kenneally <jakekenneally@gmail.com>
2017-10-24 20:24:58 -0400

    refactoring, adding build() method, rearranging alphabet to align with lights
    - also made website prettier

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Raspberry Pi Stranger Things Wall
 
+### Adapted from the Raspberry Pi Stranger Things Wall made by [CalebKussmaul](https://github.com/CalebKussmaul/Stranger-Things-Wall)
+
 Web-enabled Stranger Things inspired ouija wall using WS2811 programmable LEDs and Raspberry Pi
 
 This GIT repo contains: 
@@ -7,4 +9,3 @@ This GIT repo contains:
 * A flask server to recieve and hold messages (/server)
 * A simple HTML page with a form to send messages to the server (/website)
 * A raspberry pi client to periodically poll for messages and display them on your wall (/client)
-
--- client/stranger.py
+++ client/stranger.py
@@ -1,13 +1,12 @@
+import random
+import sys
 import time
 from datetime import datetime
-import colorsys
-import random
 from threading import Thread
-import sys
-
-import stranger_client
 
 from neopixel import *
+
+import stranger_client
 
 LED_COUNT = 50
 GPIO_PIN = 18
@@ -16,29 +15,60 @@ LED_DMA = 5
 LED_BRIGHTNESS = 255
 LED_INVERT = False
 LED_CHANNEL = 0
-LED_STRIP = ws.WS2811_STRIP_RGB   # Strip type and colour ordering
+LED_STRIP = ws.WS2811_STRIP_RGB  # Strip type and colour ordering
+
+displaying = False
 
 record_file = open("/home/pi/Desktop/stranger things/messages.txt", "a+")
 
-CHAR_IDX = {'A': 0, 'B': 2, 'C': 3, 'D': 5, 'E': 7, 'F': 9, 'G': 11, 'H': 13, 'I': 32, 'J': 30, 'K': 28, 'L': 27,
-            'M': 25, 'N': 23, 'O': 21, 'P': 19, 'Q': 18, 'R': 36, 'S': 37, 'T': 39, 'U': 40, 'V': 42, 'W': 44, 'X': 46,
-            'Y': 48, 'Z': 49, ' ': "NONE", '!': "FLASH", '*': "CREEP"}
+CHAR_IDX = {
+    'A': 48,
+    'B': 47,
+    'C': 45,
+    'D': 43,
+    'E': 42,
+    'F': 40,
+    'G': 39,
+    'H': 37,
+    'I': 20,
+    'J': 22,
+    'K': 24,
+    'L': 25,
+    'M': 26,
+    'N': 27,
+    'O': 29,
+    'P': 31,
+    'Q': 33,
+    'R': 14,
+    'S': 13,
+    'T': 12,
+    'U': 10,
+    'V': 9,
+    'W': 7,
+    'X': 5,
+    'Y': 3,
+    'Z': 1,
+    ' ': "NONE",
+    '!': "FLASH",
+    '*': "CREEP"
+}
 
 strip = Adafruit_NeoPixel(LED_COUNT, GPIO_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-#strip = Adafruit_NeoPixel(LED_COUNT, GPIO_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 strip.begin()
 strip.show()
 
 
 def rand_color():
-    return color_of(random.random())
+    return get_new_color(random.random(), False)
 
 
-def color_of(i):
+def get_new_color(i, include_blue):
     random.seed(i)
-    rgb = colorsys.hsv_to_rgb(random.random(), 1, 1)
-    return 200, int(rgb[1] * 255), int(rgb[2] * 255)
-    #return (0,250,250)
+    # blue channel doesn't work well with my lights when lighting up a single LED
+    if include_blue:
+        return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+    else:
+        return 0, random.randint(0, 255), random.randint(0, 255)
 
 
 def set_color(led, c):
@@ -50,24 +80,36 @@ def set_all(color):
         set_color(i, color)
 
 
-def set_all_color_of():
+def set_all_new_color():
     for i in range(0, LED_COUNT):
-        set_color(i, color_of(i))
+        set_color(i, get_new_color(i, True))
 
 
 def creep(n):
     for i in range(0, n):
-        set_color((i-1) % LED_COUNT, (0, 0, 0))
+        set_color((i - 1) % LED_COUNT, (0, 0, 0))
         set_color(i % LED_COUNT, rand_color())
         strip.show()
         time.sleep(1)
 
-def clearAll():
+
+def build():
+    clear_all()
+    strip.show()
+    for i in range(0, LED_COUNT):
+        set_color(i, rand_color())
+        strip.show()
+        time.sleep(.3)
+    flash(2)
+
+
+def clear_all():
     for i in range(0, LED_COUNT):
         set_all((0, 0, 0))
 
-def testAll():
-    clearAll()
+
+def test_all():
+    clear_all()
     for i in range(0, 50):
         print("setting color for {}".format(i))
         set_color(i, (0, 0, 0))
@@ -77,46 +119,44 @@ def testAll():
         time.sleep(2)
         set_color(i, (0, 0, 0))
 
+
 def flash(n):
     for i in range(0, n):
-        set_all_color_of()
+        set_all_new_color()
         strip.show()
         time.sleep(1)
-        set_all((0, 0, 0))
+        clear_all()
         strip.show()
         time.sleep(.5)
-
-displaying = False
 
 
 def display(msg):
     global displaying
     displaying = True
     for c in msg:
-        clearAll()
+        clear_all()
         if c.upper() in CHAR_IDX:
             i = CHAR_IDX[c.upper()]
             if i == "NONE":
                 "do nothing"
             elif i == "FLASH":
-                flash(5)
+                flash(3)
             elif i == "CREEP":
                 creep(50)
             else:
-                set_color(i, color_of(i))
+                set_color(i, get_new_color(i, False))
             strip.show()
             time.sleep(1)
-            set_all((0, 0, 0))
+            clear_all()
             strip.show()
             time.sleep(.2)
-    flash(2)
     time.sleep(1)
     displaying = False
 
 
 def record(msg):
-    record_file.write(str(datetime.now())+"\n")
-    record_file.write(msg+"\n\n")
+    record_file.write(str(datetime.now()) + "\n")
+    record_file.write(msg + "\n\n")
     record_file.flush()
 
 
@@ -148,16 +188,17 @@ def clear_errors():
     global displaying
     while True:
         if not displaying:
-            set_all((0, 0, 0))
+            clear_all()
         time.sleep(2)
+
 
 t0 = Thread(target=listen_on_console, args=("",))
 t1 = Thread(target=listen_on_client, args=())
 t2 = Thread(target=clear_errors, args=())
 
-#t0.start()
-#t1.start()
-#t2.start()
+t0.start()
+# t1.start()
+# t2.start()
 
 display("test test test")
 # listen_on_client()
